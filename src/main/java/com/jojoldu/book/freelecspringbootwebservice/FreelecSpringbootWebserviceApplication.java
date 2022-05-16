d7e99772b6b85cc68c4073991ad6ac559ddfc610
n0mazizi <heoyunhoe@gmail.com>
2022-05-16 10:24:26 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/book/freelecspringbootwebservice/FreelecSpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelecspringbootwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringbootWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelecSpringbootWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jojoldu/book/freelecspringbootwebservice/FreelecSpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelecspringbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringbootWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
