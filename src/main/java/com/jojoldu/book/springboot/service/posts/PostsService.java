5108639ee713dbedb158d89a4995da985e44e5ea
n0mazizi <heoyunhoe@gmail.com>
2022-05-17 09:11:17 +0900

    게시글 수정 작성

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -41,6 +41,9 @@ public class PostsService {
 
     @Transactional(readOnly = true)
     public List<PostsListResponseDto> findAllDesc() {
-        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+        return postsRepository.findAllDesc()
+                .stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
